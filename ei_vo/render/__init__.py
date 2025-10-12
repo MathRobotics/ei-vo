7c2a68061f5447e7142b7801817e4f360b3c2fbe
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-10-12 21:32:22 +0900

    Use MuJoCo default viewer for pose inspection

--- ei_vo/render/__init__.py
+++ ei_vo/render/__init__.py
@@ -1,4 +1,5 @@
+"""Rendering utilities built on top of MuJoCo."""
+
 from .render_mj import *  # noqa: F401,F403
-from .pose_viewer import inspect_pose
 
 __all__ = [name for name in globals().keys() if not name.startswith("_")]
